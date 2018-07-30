81b5f8c41778925e51b248d5f192997b79350a10
Vinson Fernandez <vinson.fernandez@admindirect.com>
2018-07-30 12:44:33 -0700

    add stage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StageComponent } from './stage/stage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StageComponent
   ],
   imports: [
     BrowserModule,
